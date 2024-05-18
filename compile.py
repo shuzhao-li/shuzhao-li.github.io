7114ddec90e222e160bc05fee0c1714ad31b87d0
Shuzhao Li <shuzhao.li@gmail.com>
2024-05-18 19:06:19 -0400

    added jinja2 time; refactor concatenate_html

--- compile.py
+++ compile.py
@@ -1,68 +1,27 @@
+import time
 import jinja2
 from bibtex import convert_bibtext_file
 
-
-def prepare_html(html_content, jinja_dict={}):
+def prepare_html(html_content, d={}):
     '''
     Open and read html_content, then update by jinja_dict.
     Returns updated HTML text
     '''
-    return html_content
+    env = jinja2.Environment()
+    template = env.from_string(html_content)
+    return template.render(d)
 
-
-# make index.html
-def make_index_html(header, footer, body, target="docs/index.html"):
-    new = prepare_html(open(header).read()
-                ) + prepare_html(open(body).read()
-                ) + prepare_html(open(footer).read()
-                )
+# make html
+def concatenate_html(header, footer, body, target="docs/index.html"):
+    new = header + prepare_html(open(body).read()) + footer
     with open(target, 'w') as F:
         F.write(new)
     
-# make publications.html
-def make_publications_html(header, footer, body, target="docs/publications.html"):
-    
-    new = prepare_html(open(header).read()
-                ) + prepare_html(open(body).read()
-                ) + prepare_html(open(footer).read()
-                )
-    with open(target, 'w') as F:
-        F.write(new)
-
-# make cv.html
-def make_cv_html(header, footer, body, target="docs/cv.html"):
-    
-    new = prepare_html(open(header).read()
-                ) + prepare_html(open(body).read()
-                ) + prepare_html(open(footer).read()
-                )
-    with open(target, 'w') as F:
-        F.write(new)
-
-# make chinese.html
-def make_chinese_html(header, footer, body, target="docs/chinese.html"):
-    
-    new = prepare_html(open(header).read()
-                ) + prepare_html(open(body).read()
-                ) + prepare_html(open(footer).read()
-                )
-    with open(target, 'w') as F:
-        F.write(new)
-
-# make posts.html
-def make_posts_html(header, footer, body, target="docs/posts.html"):
-    
-    new = prepare_html(open(header).read()
-                ) + prepare_html(open(body).read()
-                ) + prepare_html(open(footer).read()
-                )
-    with open(target, 'w') as F:
-        F.write(new)
-
-# make _publications.htm
-
 
 def main():
+    
+    header = prepare_html(open("src/_header.htm").read(), {})
+    footer = prepare_html(open("src/_footer.htm").read(), {'time_record': time.ctime()})
     
     # Convert Google Scholar exported bibtex to HTML
     with open("src/bibtex_publications.htm", 'w') as F:
@@ -71,31 +30,37 @@ def main():
             convert_bibtext_file("src/citations.txt") + '''\n</div>'''
         )
         
-    make_index_html(
-        header="src/_header.htm", footer="src/_footer.htm", body="src/_front.htm", 
+    concatenate_html(
+        header, footer, 
+        body="src/_front.htm", 
         target="docs/index.html"
         )
     
     # May change how to generate publications
-    make_publications_html(
-        header="src/_header.htm", footer="src/_footer.htm", body="src/bibtex_publications.htm", 
+    concatenate_html(
+        header, footer, body="src/bibtex_publications.htm", 
         target="docs/publications.html"
         )
-    make_posts_html(
-        header="src/_header.htm", footer="src/_footer.htm", body="src/_posts.htm", 
+    
+    # make teaching.html
+    concatenate_html(
+        header, footer, body="src/_underconstruction.htm", 
         target="docs/teaching.html"
         )
     
-    make_cv_html(
-        header="src/_header.htm", footer="src/_footer.htm", body="src/_underconstruction.htm", 
+    # make cv.html
+    concatenate_html(
+        header, footer, body="src/_underconstruction.htm", 
         target="docs/cv.html"
         )
-    make_chinese_html(
-        header="src/_header.htm", footer="src/_footer.htm", body="src/_underconstruction.htm",  
+    # make chinese.html
+    concatenate_html(
+        header, footer, body="src/_underconstruction.htm",  
         target="docs/chinese.html"
         )
-    make_posts_html(
-        header="src/_header.htm", footer="src/_footer.htm", body="src/_underconstruction.htm", 
+    # make posts.html
+    concatenate_html(
+        header, footer, body="src/_posts.htm", 
         target="docs/posts.html"
         )
     
